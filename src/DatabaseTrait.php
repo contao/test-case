c8130ac263e182b1ff92c337769c71a83bf50182
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-01-04 13:18:52 +0100

    Added DatabaseTrait

---
+++ src/DatabaseTrait.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\TestCase;
+
+trait DatabaseTrait
+{
+    private static $pdo;
+
+    protected static function loadFileIntoDatabase(string $sqlFile): void
+    {
+        $pdo = static::getConnection();
+
+        $pdo->exec(file_get_contents($sqlFile));
+    }
+
+    protected static function getConnection(): \PDO
+    {
+        if (null === self::$pdo) {
+            self::$pdo = new \PDO(
+                sprintf('mysql:host=%s;port=%s;dbname=%s;',
+                        getenv('DB_HOST'),
+                        getenv('DB_PORT'),
+                        getenv('DB_NAME')
+                ),
+                getenv('DB_USER'),
+                getenv('DB_PASS'),
+                [
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                ]
+            );
+        }
+
+        return self::$pdo;
+    }
+}

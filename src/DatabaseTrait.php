b27c3623a8bbea18dfd499467aa81762d3c3daa4
Leo Feyer <github@contao.org>
2019-01-06 18:07:21 +0100

    Throw an exception if the SQL file does not exist

--- src/DatabaseTrait.php
+++ src/DatabaseTrait.php
@@ -18,8 +18,11 @@ trait DatabaseTrait
 
     protected static function loadFileIntoDatabase(string $sqlFile): void
     {
-        $pdo = static::getConnection();
+        if (!file_exists($sqlFile)) {
+            throw new \InvalidArgumentException(sprintf('File "%s" does not exist', $sqlFile));
+        }
 
+        $pdo = static::getConnection();
         $pdo->exec(file_get_contents($sqlFile));
     }
 
@@ -27,16 +30,10 @@ trait DatabaseTrait
     {
         if (null === self::$pdo) {
             self::$pdo = new \PDO(
-                sprintf('mysql:host=%s;port=%s;dbname=%s;',
-                        getenv('DB_HOST'),
-                        getenv('DB_PORT'),
-                        getenv('DB_NAME')
-                ),
+                sprintf('mysql:host=%s;port=%s;dbname=%s;', getenv('DB_HOST'), getenv('DB_PORT'), getenv('DB_NAME')),
                 getenv('DB_USER'),
                 getenv('DB_PASS'),
-                [
-                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-                ]
+                [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]
             );
         }
 
